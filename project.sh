5a013176608dcf5312aa5d457149dd4605a7be43
Tom Brus <tombrus@gmail.com>
2020-01-03 15:20:13 +0100

    add project.sh file

---
+++ project.sh
@@ -0,0 +1,16 @@
+#===================================================================
+# general syntax:
+#  group               artifact                version     ext flags
+#===================================================================
+# flags:
+#       j = jar
+#       d = javadoc
+#       s = sources
+#===================================================================
+artifacts=(
+  "org.modelingvalue   immutable-collections   1.0.6       jar jds"
+)
+dependencies=(
+  "junit               junit                   4.12        jar jds"
+  "org.hamcrest        hamcrest-core           1.3         jar jds"
+)
