2488f07e0d72ef03695cf49a47cc664ba589a866
Tom Brus <tombrus@gmail.com>
2020-01-04 22:17:24 +0100

    update version for dependencies

--- project.sh
+++ project.sh
@@ -23,7 +23,7 @@
 #       s = sources
 #===================================================================
 artifacts=(
-  "org.modelingvalue   immutable-collections   1.0.9       jar jds"
+  "org.modelingvalue   immutable-collections   1.0.10       jar jds"
 )
 dependencies=(
   "junit               junit                   4.12        jar jds"
