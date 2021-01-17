711d3a402946eb3ac870aaa116c3e35ab8872afb
automation <automation@modelingvalue.com>
2021-01-17 12:38:05 +0000

    automatic reformat by actions

---
+++ immutable-collections/src/org/modelingvalue/collections/struct/impl/Struct10Impl.java
@@ -0,0 +1,37 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (C) Copyright 2018-2021 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
+//                                                                                                                     ~
+// Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
+// compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+// an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// specific language governing permissions and limitations under the License.                                          ~
+//                                                                                                                     ~
+// Maintainers:                                                                                                        ~
+//     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
+// Contributors:                                                                                                       ~
+//     Arjan Kok, Carel Bast                                                                                           ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+package org.modelingvalue.collections.struct.impl;
+
+import org.modelingvalue.collections.struct.Struct10;
+
+@SuppressWarnings({"unchecked", "unused"})
+public class Struct10Impl<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> extends Struct9Impl<T0, T1, T2, T3, T4, T5, T6, T7, T8> implements Struct10<T0, T1, T2, T3, T4, T5, T6, T7, T8, T9> {
+
+    private static final long serialVersionUID = 0x47114711_FD00A114L;
+
+    public Struct10Impl(T0 t0, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8, T9 t9) {
+        this((Object) t0, t1, t2, t3, t4, t5, t6, t7, t8, t9);
+    }
+
+    protected Struct10Impl(Object... data){
+        super(data);
+    }
+
+    @Override
+    public T9 get9() {
+        return (T9) get(9);
+    }
+}
---
+++ immutable-collections/src/org/modelingvalue/collections/struct/impl/Struct9Impl.java
@@ -0,0 +1,37 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (C) Copyright 2018-2021 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
+//                                                                                                                     ~
+// Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
+// compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+// an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// specific language governing permissions and limitations under the License.                                          ~
+//                                                                                                                     ~
+// Maintainers:                                                                                                        ~
+//     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
+// Contributors:                                                                                                       ~
+//     Arjan Kok, Carel Bast                                                                                           ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+package org.modelingvalue.collections.struct.impl;
+
+import org.modelingvalue.collections.struct.Struct9;
+
+@SuppressWarnings({"unchecked", "unused"})
+public class Struct9Impl<T0, T1, T2, T3, T4, T5, T6, T7, T8> extends Struct8Impl<T0, T1, T2, T3, T4, T5, T6, T7> implements Struct9<T0, T1, T2, T3, T4, T5, T6, T7, T8> {
+
+    private static final long serialVersionUID = 0x47114711_B6114B24L;
+
+    public Struct9Impl(T0 t0, T1 t1, T2 t2, T3 t3, T4 t4, T5 t5, T6 t6, T7 t7, T8 t8) {
+        this((Object) t0, t1, t2, t3, t4, t5, t6, t7, t8);
+    }
+
+    protected Struct9Impl(Object... data){
+        super(data);
+    }
+
+    @Override
+    public T8 get8() {
+        return (T8) get(8);
+    }
+}
