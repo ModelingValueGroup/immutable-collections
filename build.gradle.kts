d513ac5218d23f23563020518afe00784b9898a1
Tom Brus <tombrus@gmail.com>
2021-03-09 08:35:23 +0100

    remove comment

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,11 @@ defaultTasks("mvgCorrector", "test", "publish", "mvgTagger")
 plugins {
     `java-library`
     `maven-publish`
-    id("org.modelingvalue.gradle.mvgplugin") version "0.4.21"
+    id("org.modelingvalue.gradle.mvgplugin") version "0.4.26"
 }
 dependencies {
     annotationProcessor(project(":generator"))
     compileOnly        (project(":generator"))
-    //testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.2")
-    //testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-
 }
 publishing {
     publications {
