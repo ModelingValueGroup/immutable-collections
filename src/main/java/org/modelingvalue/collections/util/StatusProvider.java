5b7dbc33189452518298cd2c65bacd0237bcc021
WimBast <wim.bast@gmail.com>
2021-09-27 13:45:30 +0200

    do not update atomic when no status change

--- src/main/java/org/modelingvalue/collections/util/StatusProvider.java
+++ src/main/java/org/modelingvalue/collections/util/StatusProvider.java
@@ -1,3 +1,18 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (C) Copyright 2018-2021 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
+//                                                                                                                     ~
+// Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
+// compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+// an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// specific language governing permissions and limitations under the License.                                          ~
+//                                                                                                                     ~
+// Maintainers:                                                                                                        ~
+//     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
+// Contributors:                                                                                                       ~
+//     Arjan Kok, Carel Bast                                                                                           ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 package org.modelingvalue.collections.util;
 
 import java.util.Iterator;
@@ -30,7 +45,9 @@ public class StatusProvider<S extends StatusProvider.AbstractStatus> implements 
 
     public void setNext(UnaryOperator<S> nextFunction) {
         for (S pre = status.get(), post = nextFunction.apply(pre);; pre = status.get(), post = nextFunction.apply(pre)) {
-            if (status.weakCompareAndSetVolatile(pre, post)) {
+            if (post == pre) {
+                return;
+            } else if (status.weakCompareAndSetVolatile(pre, post)) {
                 pre.next.complete(post);
                 return;
             }
