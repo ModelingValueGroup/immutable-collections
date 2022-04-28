cb4e139686661c9b5c3a18d56e4d2eff3b6c4010
WimBast <wim.bast@gmail.com>
2022-04-28 16:06:40 +0200

    Serializable Predicates

---
+++ src/main/java/org/modelingvalue/collections/util/SerializablePredicate.java
@@ -0,0 +1,43 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+// (C) Copyright 2018-2022 Modeling Value Group B.V. (http://modelingvalue.org)                                        ~
+//                                                                                                                     ~
+// Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in      ~
+// compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0  ~
+// Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on ~
+// an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the  ~
+// specific language governing permissions and limitations under the License.                                          ~
+//                                                                                                                     ~
+// Maintainers:                                                                                                        ~
+//     Wim Bast, Tom Brus, Ronald Krijgsheld                                                                           ~
+// Contributors:                                                                                                       ~
+//     Arjan Kok, Carel Bast                                                                                           ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+package org.modelingvalue.collections.util;
+
+import java.util.function.Predicate;
+
+@FunctionalInterface
+public interface SerializablePredicate<U> extends Predicate<U>, LambdaReflection {
+
+    @Override
+    default SerializablePredicateImpl<U> of() {
+        return this instanceof SerializablePredicateImpl ? (SerializablePredicateImpl<U>) this : new SerializablePredicateImpl<>(this);
+    }
+
+    class SerializablePredicateImpl<U> extends LambdaImpl<SerializablePredicate<U>> implements SerializablePredicate<U> {
+
+        private static final long serialVersionUID = -8866812297241618996L;
+
+        public SerializablePredicateImpl(SerializablePredicate<U> f) {
+            super(f);
+        }
+
+        @Override
+        public final boolean test(U t) {
+            return f.test(t);
+        }
+
+    }
+
+}
