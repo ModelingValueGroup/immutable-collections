3a93eb12223a03afa7c57411945b262fbd6ca370
WimBast <wim.bast@gmail.com>
2024-11-26 14:46:06 +0100

    more serializable functions

---
+++ src/main/java/org/modelingvalue/collections/util/SerializableQuadFunction.java
@@ -0,0 +1,45 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  (C) Copyright 2018-2024 Modeling Value Group B.V. (http://modelingvalue.org)                                         ~
+//                                                                                                                       ~
+//  Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in       ~
+//  compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0   ~
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on  ~
+//  an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the   ~
+//  specific language governing permissions and limitations under the License.                                           ~
+//                                                                                                                       ~
+//  Maintainers:                                                                                                         ~
+//      Wim Bast, Tom Brus                                                                                               ~
+//                                                                                                                       ~
+//  Contributors:                                                                                                        ~
+//      Ronald Krijgsheld ✝, Arjan Kok, Carel Bast                                                                       ~
+// --------------------------------------------------------------------------------------------------------------------- ~
+//  In Memory of Ronald Krijgsheld, 1972 - 2023                                                                          ~
+//      Ronald was suddenly and unexpectedly taken from us. He was not only our long-term colleague and team member      ~
+//      but also our friend. "He will live on in many of the lines of code you see below."                               ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+package org.modelingvalue.collections.util;
+
+@FunctionalInterface
+public interface SerializableQuadFunction<A, B, C, D, E> extends QuadFunction<A, B, C, D, E>, LambdaReflection {
+
+    @Override
+    default SerializableQuadFunctionImpl<A, B, C, D, E> of() {
+        return this instanceof SerializableQuadFunctionImpl ? (SerializableQuadFunctionImpl<A, B, C, D, E>) this : new SerializableQuadFunctionImpl<>(this);
+    }
+
+    class SerializableQuadFunctionImpl<A, B, C, D, E> extends LambdaImpl<SerializableQuadFunction<A, B, C, D, E>> implements SerializableQuadFunction<A, B, C, D, E> {
+        private static final long serialVersionUID = 4050070048423930858L;
+
+        public SerializableQuadFunctionImpl(SerializableQuadFunction<A, B, C, D, E> f) {
+            super(f);
+        }
+
+        @Override
+        public E apply(A s, B t, C u, D v) {
+            return f.apply(s, t, u, v);
+        }
+
+    }
+
+}
---
+++ src/main/java/org/modelingvalue/collections/util/SerializableSupplier.java
@@ -0,0 +1,46 @@
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//  (C) Copyright 2018-2024 Modeling Value Group B.V. (http://modelingvalue.org)                                         ~
+//                                                                                                                       ~
+//  Licensed under the GNU Lesser General Public License v3.0 (the 'License'). You may not use this file except in       ~
+//  compliance with the License. You may obtain a copy of the License at: https://choosealicense.com/licenses/lgpl-3.0   ~
+//  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on  ~
+//  an 'AS IS' BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the   ~
+//  specific language governing permissions and limitations under the License.                                           ~
+//                                                                                                                       ~
+//  Maintainers:                                                                                                         ~
+//      Wim Bast, Tom Brus                                                                                               ~
+//                                                                                                                       ~
+//  Contributors:                                                                                                        ~
+//      Ronald Krijgsheld ✝, Arjan Kok, Carel Bast                                                                       ~
+// --------------------------------------------------------------------------------------------------------------------- ~
+//  In Memory of Ronald Krijgsheld, 1972 - 2023                                                                          ~
+//      Ronald was suddenly and unexpectedly taken from us. He was not only our long-term colleague and team member      ~
+//      but also our friend. "He will live on in many of the lines of code you see below."                               ~
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+package org.modelingvalue.collections.util;
+
+import java.util.function.Supplier;
+
+@FunctionalInterface
+public interface SerializableSupplier<V> extends Supplier<V>, LambdaReflection {
+
+    @Override
+    default SerializableSupplierImpl<V> of() {
+        return this instanceof SerializableSupplierImpl ? (SerializableSupplierImpl<V>) this : new SerializableSupplierImpl<>(this);
+    }
+
+    class SerializableSupplierImpl<V> extends LambdaImpl<SerializableSupplier<V>> implements SerializableSupplier<V> {
+        private static final long serialVersionUID = 2689432770683659791L;
+
+        public SerializableSupplierImpl(SerializableSupplier<V> f) {
+            super(f);
+        }
+
+        @Override
+        public final V get() {
+            return f.get();
+        }
+    }
+
+}
